290d38e327ad9cf7a9d62b058afa8167005a1a75
Elias Roussos <eliasIsaiah@outlook.com>
2020-05-24 00:42:47 -0400

    ending on section 3 'conditional rendering in JSX'

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,80 @@
-'use strict';
+"use strict";
 
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
+// only render the subtitle (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "here are your options" "No Options"
+
+var article = {
+    title: "To Kill a Mockingbird",
+    subtitle: "The Modern Prometheus",
+    options: ['One', 'Two']
+};
+
+var title = article.title,
+    subtitle = article.subtitle,
+    options = article.options;
+
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        title
+    ),
+    subtitle && React.createElement(
+        "p",
+        null,
+        subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        options.length > 0 ? 'here are your options' : 'No Options'
+    )
 );
+
+var user = {
+    name: 'Elias',
+    age: 24,
+    userLocation: 'Rochester'
+};
+
+var name = user.name,
+    age = user.age,
+    userLocation = user.userLocation;
+
+
+var getLocation = function getLocation() {
+    if (userLocation) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            userLocation
+        );
+    }
+};
+
+var templateElias = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        name ? name : 'undefined'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,47 @@
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-const template = <h1>Indecision App</h1>;
+// only render the subtitle (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "here are your options" "No Options"
+
+const article = {
+    title: "To Kill a Mockingbird",
+    subtitle: "The Modern Prometheus",
+    options: ['One', 'Two']
+}
+
+const {title, subtitle, options} = article;
+
+const template = (
+    <div>
+        <h1>{title}</h1>
+        {subtitle && <p>{subtitle}</p>}
+        <p>{options.length > 0 ? 'here are your options' : 'No Options'}</p>
+    </div>
+);
+
+
+const user = {
+    name: 'Elias',
+    age: 24,
+    userLocation: 'Rochester'
+};
+
+const { name, age, userLocation } = user;
+
+const getLocation = () => {
+    if (userLocation) {
+        return <p>Location: {userLocation}</p>;
+    }
+}
+
+const templateElias = (
+    <div>
+        <h1>{name ? name : 'undefined'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
